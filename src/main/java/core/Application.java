beba19592fc1dd816f76455e3b5709340dc473bd
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-23 19:42:17 +0100

    test the application

--- src/main/java/core/Application.java
+++ src/main/java/core/Application.java
@@ -1,2 +1,28 @@
-package core;public class Application {
+package core;
+
+import entity.Personne;
+import service.JpaService;
+
+import java.util.List;
+
+public class Application {
+    private static final JpaService jpaService = JpaService.getInstance();
+
+    public static void main(String[] args) {
+        printPersonData();
+
+    }
+
+    // print persone data
+    public static void printPersonData() {
+        List<Personne> personneList =jpaService
+        .runInTransaction(entityManager ->
+            entityManager.createQuery("select p from Personne p", Personne.class)
+                    .getResultList());
+        personneList.stream()
+                .map(personne -> "Full Name: " + personne.getFullname() + " Email" + personne.getEmail())
+                .forEach(System.out::println);
+
+    }
+
 }

869fcfbdb5b642179541ccfde1a7c59773761b3e
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-24 17:26:18 +0100

    Creat simple local storag

--- src/main/java/service/LocalStore.java
+++ src/main/java/service/LocalStore.java
@@ -1,2 +1,41 @@
-package service;public class localStore {
+package service;
+
+import entity.Store;
+
+public class LocalStore {
+    private  static final Store entity = new Store();
+
+    public static void setItem(String key, String value) {
+        entity.setKey(key);
+        entity.setValue(value);
+        System.out.println(entity.getKey()+ "****** " + entity.getValue());
+        JpaService.getInstance().runInTransaction(entityManager -> {
+            entityManager.persist(entity);
+            return null;
+        });
+    }
+    public static String getItem(String key) {
+        String value = null;
+        value = JpaService.getInstance().runInTransaction(entityManager -> {
+            return entityManager.createQuery("select l from Store  l where l.key = :key order by l.id desc", Store.class)
+                   // if exist return the last value if not return null
+                    .setParameter("key", key)
+                    .getResultList().size() > 0 ? entityManager.createQuery("select l from Store l where l.key = :key order by l.id desc", Store.class)
+                    .setParameter("key", key)
+                    .getResultList().get(0).getValue() : null;
+
+        });
+
+        return value;
+    }
+
+    public static void removeItem(String key) {
+        JpaService.getInstance().runInTransaction(entityManager -> {
+            entityManager.createQuery("delete from Store l where l.key = :key")
+                    .setParameter("key", key)
+                    .executeUpdate();
+            return null;
+        });
+    }
+
 }

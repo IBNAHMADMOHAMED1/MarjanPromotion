cfda1cf12460e17216b36b8bd0a82b0ebadb073d
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-27 16:56:41 +0100

    modification de la classe PromotionController

--- src/main/java/controller/PromotionController.java
+++ src/main/java/controller/PromotionController.java
@@ -1,2 +1,24 @@
-package controller;public class PromotionController {
+package controller;
+
+import dao.HibernateDao;
+import entity.Promotion;
+import service.JpaService;
+
+public class PromotionController extends HibernateDao<Promotion> {
+    private static final JpaService jpaService = JpaService.getInstance();
+    public PromotionController() {
+        setClazz(Promotion.class);
+    }
+
+    // create promotion
+    public Promotion createPromotion(String nom, String dateDebut, String dateFin, Double discount, int idCategorie ) {
+        Promotion promotion = new Promotion();
+        promotion.setCategorieId(idCategorie);
+        promotion.setNompromotion(nom);
+        promotion.setIsaccepted(false);
+        promotion.setEnddate(dateFin);
+        promotion.setStartdate(dateDebut);
+        promotion.setRatio(discount);
+        return create(promotion);
+    }
 }

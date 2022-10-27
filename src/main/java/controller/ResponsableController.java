6a648f985de01e9d3e695b41899333c09e1bad4e
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-27 16:56:41 +0100

    modification de la classe ResponsableController

--- src/main/java/controller/ResponsableController.java
+++ src/main/java/controller/ResponsableController.java
@@ -1,2 +1,28 @@
-package controller;public class ResponsableController {
+package controller;
+
+import dao.HibernateDao;
+import entity.Categorie;
+import entity.Personne;
+import entity.Responsable;
+import service.JpaService;
+
+public class ResponsableController extends HibernateDao<Responsable> {
+    private static Responsable entity = new Responsable();
+
+    private static final JpaService jpaService = JpaService.getInstance();
+    public ResponsableController() {
+        setClazz(Responsable.class);
+    }
+
+    // create responsable
+    public Responsable createResponsable(int personneId, int categorieId, int centreId) {
+       Responsable responsable = new Responsable();
+        CategorieController cg = new CategorieController();
+         responsable.setPersonneId(personneId);
+         responsable.setCategorieId(categorieId);
+         responsable.setIdcentre(centreId);
+        Responsable rs = create(responsable);
+         cg.updateIdResponsable(categorieId, rs.getIdresponsable());
+          return  rs;
+    }
 }

225b7bcdd9e1009d87754c1682168d216090f82b
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-27 16:56:42 +0100

    modification de la classe Centre

--- src/main/java/entity/Centre.java
+++ src/main/java/entity/Centre.java
@@ -2,8 +2,10 @@ package entity;
 
 import jakarta.persistence.*;
 
+import java.io.Serializable;
+
 @Entity
-public class Centre {
+public class Centre implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     @Column(name = "idcentre")

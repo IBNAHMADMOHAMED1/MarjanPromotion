01fbe5ceaaf22b94f676ea8a9c34b8fcb4d13027
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-27 16:56:40 +0100

    modification de la classe CategorieController

--- src/main/java/controller/CategorieController.java
+++ src/main/java/controller/CategorieController.java
@@ -1,2 +1,34 @@
-package controller;public class CategorieController {
+package controller;
+
+import dao.HibernateDao;
+import entity.Categorie;
+import service.JpaService;
+
+import java.util.List;
+
+public class CategorieController extends HibernateDao<Categorie> {
+    private static final JpaService jpaService = JpaService.getInstance();
+    private static Categorie entity = new Categorie();
+    public CategorieController() {
+        setClazz(Categorie.class);
+    }
+
+    // getAllCategoriesByCentre
+    public List<Categorie> getAllCategoriesByCentre(int idCentre, String type) {
+        List<Categorie>  list = (List<Categorie>) jpaService.runInTransaction(entityManager -> entityManager
+                .createNativeQuery("select * from categorie where idCentre = " + idCentre, Categorie.class)
+                .getResultList());
+        if (type.equals("all")){
+            System.out.println(idCentre);
+            return list;
+        }
+        list.removeIf(categorie -> categorie.getIdresponsable() != null);
+        return list;
+    }
+
+    public void  updateIdResponsable(int idCategorie, int idResponsable) {
+        jpaService.runInTransaction(entityManager -> entityManager
+                .createQuery("update Categorie set idresponsable = " + idResponsable + " where idcategorie = " + idCategorie)
+                .executeUpdate());
+    }
 }

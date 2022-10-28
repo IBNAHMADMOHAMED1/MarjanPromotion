2c40a30e8f3363968d90bf54e2ef4bcc9c634bfc
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:20 +0100

    Creation de la classe test unitaire AdminVilleControllerTest

--- src/test/java/controller/AdminVilleControllerTest.java
+++ src/test/java/controller/AdminVilleControllerTest.java
@@ -1,4 +1,14 @@
+package controller;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AdminVilleControllerTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void getAllAdminVille() {
+        AdminVilleController adminVilleController = new AdminVilleController();
+        adminVilleController.getAllAdminVille();
+    }
+
 }
+

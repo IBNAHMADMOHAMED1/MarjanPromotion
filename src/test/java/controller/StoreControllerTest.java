bb234340c5b6ecf89e7983872972e8df270aaa72
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:21 +0100

    Creation de la classe test unitaire StoreControllerTest

--- src/test/java/controller/StoreControllerTest.java
+++ src/test/java/controller/StoreControllerTest.java
@@ -1,4 +1,24 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StoreControllerTest {
-  
+
+    @Test
+    void setItem() {
+    }
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void deleteItem() {
+    }
 }

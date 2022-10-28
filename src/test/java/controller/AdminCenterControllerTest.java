37f6d64136d4021c406e71ec543a5f7602d56e01
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:20 +0100

    Creation de la classe test unitaire AdminCenterControllerTest

--- src/test/java/controller/AdminCenterControllerTest.java
+++ src/test/java/controller/AdminCenterControllerTest.java
@@ -1,4 +1,14 @@
+package controller;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AdminCenterControllerTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void getAdminCentreByPersonne() {
+        AdminCenterController adminCenterController = new AdminCenterController();
+        adminCenterController.getAdminCentreByPersonne(1);
+    }
 }
+
+

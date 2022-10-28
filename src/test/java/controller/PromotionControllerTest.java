c0a779f6f3f28b165099106983a08ba6252b6a0c
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:21 +0100

    Creation de la classe test unitaire PromotionControllerTest

--- src/test/java/controller/PromotionControllerTest.java
+++ src/test/java/controller/PromotionControllerTest.java
@@ -1,4 +1,20 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PromotionControllerTest {
-  
+
+    @Test
+    void createPromotion() {
+    }
+
+    @Test
+    void getPromotionByCategorie() {
+    }
+
+    @Test
+    void validatePromotion() {
+    }
 }

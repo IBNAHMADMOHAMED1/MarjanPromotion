f95c1c932703abf635ec65f6d20005c1130e6342
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:20 +0100

    Creation de la classe test unitaire CategorieControllerTest

--- src/test/java/controller/CategorieControllerTest.java
+++ src/test/java/controller/CategorieControllerTest.java
@@ -1,4 +1,25 @@
+package controller;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CategorieControllerTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void getAllCategoriesByCentre() {
+        CategorieController categorieController = new CategorieController();
+        categorieController.getAllCategoriesByCentre(1, "all");
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateIdResponsable() {
+        CategorieController categorieController = new CategorieController();
+        categorieController.updateIdResponsable(1, 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCategorieByResponsable() {
+        CategorieController categorieController = new CategorieController();
+        categorieController.getCategorieByResponsable(1);
+    }
 }
+

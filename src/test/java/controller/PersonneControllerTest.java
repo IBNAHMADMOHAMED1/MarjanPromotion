16426f7c53d58853909ddd2364501db50ff6203f
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:20 +0100

    Creation de la classe test unitaire PersonneControllerTest

--- src/test/java/controller/PersonneControllerTest.java
+++ src/test/java/controller/PersonneControllerTest.java
@@ -1,4 +1,60 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PersonneControllerTest {
-  
+
+    @Test
+    void getFullname() {
+    }
+
+    @Test
+    void getIsLogin() {
+    }
+
+    @Test
+    void setIsLogin() {
+    }
+
+    @Test
+    void getEntity() {
+    }
+
+    @Test
+    void setEntity() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void storeToken() {
+    }
+
+    @Test
+    void checkToken() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void createPersonne() {
+    }
+
+    @Test
+    void findAllPersonne() {
+    }
+
+    @Test
+    void findPersonneById() {
+    }
+
+    @Test
+    void createPersonneDetails() {
+    }
 }

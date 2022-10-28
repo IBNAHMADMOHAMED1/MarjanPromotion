3064d0c35bfd2f687f2efd6fee319100a1fadc64
IBNAHMADMOHAMED1 <ibnahamdmohamed8@gmail.com>
2022-10-28 10:08:21 +0100

    Creation de la classe test unitaire VilleControllerTest

--- src/test/java/controller/VilleControllerTest.java
+++ src/test/java/controller/VilleControllerTest.java
@@ -1,4 +1,36 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class VilleControllerTest {
-  
+
+    @Test
+    void getEntity() {
+    }
+
+    @Test
+    void setEntity() {
+    }
+
+    @Test
+    void getAllVille() {
+    }
+
+    @Test
+    void createVille() {
+    }
+
+    @Test
+    void chekIfVilleExist() {
+    }
+
+    @Test
+    void getVilleById() {
+    }
+
+    @Test
+    void checkIfVilleHasAdmin() {
+    }
 }
